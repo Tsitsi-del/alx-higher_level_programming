b48feec88b67a184c0408630a5456d5622145aea
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-07-10 06:56:04 -0700

    My python files

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,30 @@
+#include "list.h"
+
+/**
+ * check_cycle - function to check if the linked list contain a cycle
+ * @list: list to check
+ * Return: 1 list is a cycle, 0 if it's not a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slw = list;
+	listint_t *fast = list;
+
+	if (list == NULL)
+	{
+		return (0);
+	}
+
+	while (slw && fast && fast ->next)
+	{
+		slw = slw->next;
+		fast = fast->next->next;
+
+		if (slw == fast)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
