9f93839cc3b24ffd924b0667e708ec8b21302ca9
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-08-02 06:48:46 -0700

    python classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-"""Clas named Square is defined"""
+"""Class named Square is defined"""
 
 
 class Square:
+    """a function called __init__ is defined"""
+
     def __init__(self, size):
+        """initializes __size of size and self"""
         self.__size = size
