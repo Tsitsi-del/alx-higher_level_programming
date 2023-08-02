fa973504788bca942a11c079ac46b344164b6fc3
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-08-02 06:53:14 -0700

    python classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
+"""Class named Square is defined"""
+
 
 class Square:
+    """function named __init__ defined"""
     def __init__(self, size=0):
+        """if statement"""
         if type(size) != int:
+            """raise error"""
             raise TypeError("size must be an integer")
         elif size < 0:
+            """raise error"""
             raise ValueError("size must be >= 0")
         else:
+            """initialize __size of size and self"""
             self.__size = size
