f020117a26d518668c65f0072b5b3d3276fc09b5
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-08-02 15:05:01 -0700

    python classes

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,20 +1,43 @@
 #!/usr/bin/python3
+"""class square defined"""
+
 
 class Square:
+    """represents a square
+    Attributes:
+        __size (int): size of a size of square
+        __position (tuple): position of square in 2D space"""
 
     def __init__(self, size=0, position=(0, 0)):
+        """initializes square
+        Args:
+            size (int): size of side of square
+            position (tuple): position of square
+        Returns:
+            None"""
         self.size = size
         self.position = position
 
     def area(self):
+        """square area function
+        Returns:
+            area of the square"""
         return (self.__size) ** 2
 
     @property
     def size(self):
+        """getter of __size
+        Returns:
+            size of square"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """setter of __size
+        Args:
+            value (int): size of side of square
+        Returns:
+            None"""
         if type(value) is not int:
             raise TypeError("size must be an integer")
         else:
@@ -24,6 +47,9 @@ class Square:
                 self.__size = value
 
     def my_print(self):
+        """print square
+        Returns:
+            None"""
         if self.__size == 0:
             print()
             return
@@ -35,10 +61,18 @@ class Square:
 
     @property
     def position(self):
+        """getter of __position
+        Returns:
+            position of square"""
         return self.__position
 
     @position.setter
     def position(self, value):
+        """setter of __position
+        Args:
+            value (tuple): position of square
+        Returns:
+            None"""
         if type(value) is not tuple or len(value) != 2 or \
                 type(value[0]) is not int or value[0] < 0 or \
                 type(value[1]) is not int or value[1] < 0:
