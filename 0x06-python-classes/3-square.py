7ca635945030a7a20dfed7302c4099e93d4885b8
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-08-02 06:59:24 -0700

    python classes

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
+"""class square is defined"""
+
 
 class Square:
+    """__init__ function is defined"""
     def __init__(self, size=0):
+        """if statement"""
         if type(size) is not int:
+            """raise error"""
             raise TypeError("size must be an integer")
         elif size < 0:
+            """raise error"""
             raise ValueError("size must be >= 0")
         else:
+            """initialize"""
             self.__size = size
-
+    """area function defined"""
     def area(self):
+        """returns area"""
         return self.__size ** 2
