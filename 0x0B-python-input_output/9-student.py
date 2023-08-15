6630ccd839594c1e9082f3760d33fab8de16eb77
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-08-15 08:22:03 -0700

    task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -9,22 +9,22 @@ class Student:
     Class Student
     """
     def __init__(self, first_name, last_name, age):
-    """
-    Class Student
-    Attributes:
-        first_name (str): student name
-        last_name (str): last name of student
-        age (int): student age
-    Methods:
-        __init__ - initialize student instance
-        to_json - dictionary of student instance
-    """
+        """
+        Class Student
+        Attributes:
+            first_name (str): student name
+            last_name (str): last name of student
+            age (int): student age
+        Methods:
+            __init__ - initialize student instance
+            to_json - dictionary of student instance
+        """
         self.first_name = first_ name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-    """
-    Dictionary of student insatnce
-    """
+        """
+        Dictionary of student insatnce
+        """
         return self.__dict__
