ffa2f630b25a16bed89d648142c58c522490564c
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-08-15 12:07:08 -0700

    task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Function for pascal triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    returns list of lists of integers
+    """
+    if n <= 0:
+        return []
+
+    tri = []
+
+    for x in range(n):
+        rw = [1]
+        if tri:
+            p_rw = tri[-1]
+            for k in range(len(p_rw) - 1):
+                rw.append(p_rw[k] + p_rw[k + 1])
+                rw.append(1)
+        tri.append(rw)
+    return tri
