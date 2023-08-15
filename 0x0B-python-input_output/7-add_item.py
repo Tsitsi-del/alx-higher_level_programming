7b7a787ff75d98bd9fa99de7432148473705e591
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-08-15 07:21:44 -0700

    task7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,8 +2,8 @@
 """
 7-add_item.py
 """
-import json
 import sys
+import json
 import os.path
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
