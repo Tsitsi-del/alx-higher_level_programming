a71861c80612ac4201881e949ad6c6aef90981c4
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-08-15 11:00:08 -0700

    task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,30 +1,23 @@
 #!/usr/bin/python3
 """
-    Class Student
+Function for class student
 """
 
 
 class Student:
     """
-        Class Student defines student by:
-        Attributes:
-            first_name (str): student name
-            last_name (str): student name
-            age (int): student age
-        Methods:
-            __init__ - initializes Student instance
-            to_json - retrieve dictionary of Student Instance
+    Student class
     """
     def __init__(self, first_name, last_name, age):
         """
-            Initialize Student instance
+        instance attributes
         """
-        self.first_name = first_ name
+        self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
         """
-            Dictionary of student instance.
+        retrieves a dictionary representation of Student instance
         """
         return self.__dict__
