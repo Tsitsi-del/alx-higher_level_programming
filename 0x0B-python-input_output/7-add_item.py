7e2062943d7e94283177135d46e526562c263001
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-08-15 07:15:23 -0700

    task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+7-add_item.py
+"""
+import json
+import sys
+import os.path
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+file_name = "add_item.json"
+json_list = []
+
+if os.path.exists(file_name):
+    json_list = load_from_json_file(file_name)
+
+for index in range(1, len(sys.argv)):
+    json_list.append(sys.argv[index])
+
+save_to_json_file(json_list, file_name)
