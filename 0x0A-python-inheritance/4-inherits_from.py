7eeab56f432da28efca6e363972ef23e0150523a
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-08-16 06:34:31 -0700

    task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Function inherits_from
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Returns True if the object is an instance 
+    if a class that inherited from a class
+    """
+    if type(obj) is a_class:
+        return False
+    return issubclass(type(obj), a_class)
