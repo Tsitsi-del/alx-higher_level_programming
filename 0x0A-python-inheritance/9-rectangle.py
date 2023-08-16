47131aa861dc293354bb6244e570944eca7e7f11
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-08-16 12:15:17 -0700

    task 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+class Rectangle
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    class rectangle
+    """
+    def __init__(self, width, height):
+        """
+        instances of rectangle
+        """
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        """
+        calculates area
+        """
+        return self.__width * self.__height
+    def __str__(self):
+        """
+        string representation
+        """
+        return (f"[Rectangle] {self.__width}/{self.__height"})
