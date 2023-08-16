eec21d0bd68f591d23d8b090fef94161ae8ad46d
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-08-16 06:44:35 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class
+    """
+    def area(self):
+        """
+        function area
+        """
+        raise Exception("area() is not implemented")
