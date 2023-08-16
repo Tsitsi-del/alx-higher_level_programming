419ca9256cd4f5f4fa55b9e98c0fa7c9847d3da3
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-08-16 12:31:49 -0700

    task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Class Square
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class Square
+    """
+
+    def __init__(self, size):
+        """
+        instances of class square
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """
+        calculates area
+        """
+        return self.__size * self.__size
