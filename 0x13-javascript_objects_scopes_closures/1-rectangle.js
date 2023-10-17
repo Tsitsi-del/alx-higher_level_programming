9e909958b1ea21069896bc4f0e15a8a9e36436cf
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-10-17 04:40:06 -0700

    task 1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+ * Rectangle class with height and width attributes
+ */
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
