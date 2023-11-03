4986154f6ef8a14358620dc41327ad7978410d2f
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-11-03 16:22:19 +0200

    task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Sends a request to a given URL and display the response body"""
+import sys
+import urllib.request
+import urllib.error
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+        except urllib.error.HTTPError as e:
+            print("Error code: {}".format(e.code))
